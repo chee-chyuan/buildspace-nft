e0ea0c2c38340999865f0596831c64336d478d03
chee-chyuan <60590919+chee-chyuan@users.noreply.github.com>
2021-11-21 17:33:13 +0800

    init project

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
+let fs = require('fs');
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+let infuraUrl = "https://eth-rinkeby.alchemyapi.io/v2/YsxGvGCBzhQoawf6U-Y5euJ3NEUaV2cD";
+let privateKey = fs.readFileSync("./privatekey/kovanPrivateKey.txt").toString().trim();
+let etherscanApiKey = fs.readFileSync("./privatekey/etherscanApiKey.txt").toString().trim();
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: infuraUrl,
+      accounts: [`0x${privateKey}`],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API,
+  }
+
+};
+
